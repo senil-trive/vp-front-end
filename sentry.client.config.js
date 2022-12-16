b3c2c9e7dadf1976f63e1b5dd0d98747bffdae20
Elton Gonçalves Gomes <eltongonc@gmail.com>
2022-12-16 11:55:01 +0100

    updated the code

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -6,14 +6,12 @@ import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1.0,
-    // ...
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
-  });
-}
+Sentry.init({
+  dsn: SENTRY_DSN || "https://405c43babb0f4574b9a56885f537d50b@o4504337991991296.ingest.sentry.io/4504337993498624",,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,9 +6,8 @@ import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-if (SENTRY_DSN) {
   Sentry.init({
-    dsn: SENTRY_DSN,
+    dsn: SENTRY_DSN || "https://405c43babb0f4574b9a56885f537d50b@o4504337991991296.ingest.sentry.io/4504337993498624",,
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
     // ...
@@ -16,4 +15,3 @@ if (SENTRY_DSN) {
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
     // that it will also get attached to your source maps
   });
-}
