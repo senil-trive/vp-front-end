65c1946cf9e48406913f1d8c3ef577cf59cb4039
Elton Gonçalves Gomes <eltongonc@gmail.com>
2022-12-16 15:37:14 +0100

    updated the error.tsx file

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -30,7 +30,7 @@ const Error = (props: any) => {
 Error.getInitialProps = async (contextData: any) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
-  await Sentry.captureUnderscoreErrorException(contextData);
+  await Sentry.captureException(contextData);
 
   // This will contain the status code of the response
   return NextErrorComponent.getInitialProps(contextData);
