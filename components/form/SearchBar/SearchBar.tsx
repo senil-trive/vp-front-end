feaaf710c5d6e128bf64f5ac692b3058c3cb67ea
Elton Gonçalves Gomes <eltongonc@gmail.com>
2023-03-10 14:49:27 +0100

    updated the collection searchbar

--- components/form/SearchBar/SearchBar.tsx
+++ components/form/SearchBar/SearchBar.tsx
@@ -1,10 +1,17 @@
 import { debounce } from "@mui/material";
 import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
+import { useTheme } from "styled-components";
+import { ColorType } from "../../../types/colorTypes";
 import SearchIcon from "../../icons/SearchIcon/SearchIcon";
 import Input from "../Input/Input";
 
 type Props = {
+  iconColor?: ColorType;
+
+  /** The placeholder of the input field */
+  placeholder?: string;
+
   /** The time the field will wait before triggering the callback */
   waitTime?: number;
 
@@ -12,7 +19,13 @@ type Props = {
   onSearch?: (x: string) => void;
 };
 
-export default function SearchBar({ onSearch, waitTime = 300 }: Props) {
+export default function SearchBar({
+  iconColor = "primary",
+  placeholder = "Zoeken...",
+  onSearch,
+  waitTime = 300,
+}: Props) {
+  const { colors } = useTheme();
   const router = useRouter();
   const [val, setVal] = useState("");
 
@@ -31,8 +44,8 @@ export default function SearchBar({ onSearch, waitTime = 300 }: Props) {
 
   return (
     <Input
-      iconLeft={<SearchIcon />}
-      placeholder="Zoeken..."
+      iconLeft={<SearchIcon color={colors[iconColor]} />}
+      placeholder={placeholder}
       defaultValue={val}
       onChange={debouncedSearch}
       borderColor="grey"
