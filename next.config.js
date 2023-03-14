a12505583f0f0fb4ff18b4631747a8023f01c8b2
Elton Gonçalves Gomes <eltongonc@gmail.com>
2023-03-14 10:32:53 +0100

    added redirects for open-brieven

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ const moduleExports = {
         destination: "/kinderen/verhalen/:path*",
         permanent: false,
       },
+
       /**
        * Redirects for forum
        */
@@ -41,6 +42,40 @@ const moduleExports = {
         destination: "/kinderen/forum/:path*",
         permanent: false,
       },
+
+      /**
+       * Redirects for open brieven
+       */
+      {
+        source: "/open-brieven/",
+        destination: "/kinderen/open-brieven/",
+        permanent: false,
+      },
+      {
+        source: "/open-brief-aan-alle-kinderen-van-gescheiden-ouders/",
+        destination:
+          "/kinderen/open-brieven/aan-alle-kinderen-met-gescheiden-ouders",
+        permanent: false,
+      },
+      {
+        source: "/open-brief-aan-alle-gescheiden-ouders/",
+        destination: "/kinderen/open-brieven/voor-je-ouders/",
+        permanent: false,
+      },
+      {
+        source: "/open-brief-aan-alle-nieuwe-partners-van-gescheiden-ouders/",
+        destination:
+          "/kinderen/open-brieven/voor-de-nieuwe-partner-van-papa-of-mama/",
+        permanent: false,
+      },
+
+      // One open-brieven leads directly to a pdf file
+      // {
+      //   source:
+      //     "https://www.villapinedo.nl/wp-content/uploads/2022/07/Open-Brief-Villa-Pinedo-Aan-alle-studenten-met-gescheiden-ouders.pdf",
+      //   destination: "/kinderen/open-brieven/",
+      //   permanent: false,
+      // },
     ];
   },
 };
