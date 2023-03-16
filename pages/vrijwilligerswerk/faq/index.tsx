6720fe6d0e418084cb9e73866689636160a9d209
Elton Gonçalves Gomes <eltongonc@gmail.com>
2023-03-16 11:30:02 +0100

    refactored the faq page

--- pages/vrijwilligerswerk/faq/index.tsx
+++ pages/vrijwilligerswerk/faq/index.tsx
@@ -1,25 +1,21 @@
 import { H1, H3, P } from "../../../components/typography";
 
-import { Container } from "@mui/material";
-import ENDPOINTS from "../../../constants/endpoints";
+import { CircularProgress, Container } from "@mui/material";
 import { FAQ } from "../../../types/content-types/FAQ.type";
 import FAQItem from "../../../components/content-types/FAQItem/FAQItem";
 import { Hero } from "../../../components/layout";
 import PageWrapper from "../../../components/layout/PageWrapper/PageWrapper";
 import { getFaqOverviewData, getFaqs } from "../../../utils/api";
+import { useState } from "react";
+import { VolunteersFAQPageProps } from "../../../types/pageTypes";
+import Button from "../../../components/buttons/Button";
 
-type VolunteersFAQPageProps = {
-  pageData: any;
-  faqData: FAQ[];
-  error?: boolean;
-};
+const POST_PER_PAGE = 6;
 
 export const getServerSideProps = async () => {
-  // fetch page data from API
-
   try {
     const pageReq = await getFaqOverviewData();
-    const faqReq = await getFaqs({ postPerPage: 7 });
+    const faqReq = await getFaqs({ postPerPage: POST_PER_PAGE });
 
     const pageRes = await pageReq.json();
     const faqRes = await faqReq.json();
@@ -28,6 +24,7 @@ export const getServerSideProps = async () => {
       props: {
         pageData: pageRes.data,
         faqData: faqRes.data,
+        totalFaqs: faqRes?.meta?.total_count ?? 0,
       },
     };
   } catch (error) {
@@ -44,7 +41,37 @@ export const getServerSideProps = async () => {
 const VolunteersFAQPage: React.FC<VolunteersFAQPageProps> = ({
   pageData,
   faqData,
+  totalFaqs,
 }) => {
+  const [items, setItems] = useState(faqData);
+  const [totalCount, setTotalCount] = useState(totalFaqs);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const getPaginatedPost = async () => {
+    setIsLoading(true);
+    try {
+      const req = await getFaqs({
+        postPerPage: POST_PER_PAGE,
+        page: currentPage,
+        meta: "filter_count",
+      });
+      const res = await req.json();
+
+      setItems([...items, ...(res.data ?? [])]);
+      setTotalCount(res?.meta?.filter_count || 0);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const changePage = () => {
+    setCurrentPage(currentPage + 1);
+    getPaginatedPost();
+  };
+
   return (
     <div>
       <PageWrapper
@@ -59,7 +86,10 @@ const VolunteersFAQPage: React.FC<VolunteersFAQPageProps> = ({
             </div>
           </Hero>
 
-          <section className="my-[80px] text-center py-20">
+          <section
+            className="mt-[80px] text-center"
+            style={{ marginBottom: totalCount > items.length ? 56 : 80 }}
+          >
             <Container>
               <div className="flex flex-col items-center justify-center mb-14">
                 <H3 variant="bold" color="black">
@@ -70,7 +100,7 @@ const VolunteersFAQPage: React.FC<VolunteersFAQPageProps> = ({
 
             <Container>
               <div className="flex flex-col gap-8">
-                {faqData?.map((faq: FAQ) => (
+                {items?.map((faq: FAQ) => (
                   <FAQItem
                     key={faq.id}
                     title={faq.title}
@@ -79,7 +109,16 @@ const VolunteersFAQPage: React.FC<VolunteersFAQPageProps> = ({
                 ))}
               </div>
             </Container>
+            <div className="flex items-center justify-center">
+              {isLoading && <CircularProgress size={"30px"} />}
+            </div>
           </section>
+
+          {totalCount > items.length && (
+            <Container style={{ marginBottom: 80 }}>
+              <Button onClick={changePage}>Meer laden</Button>
+            </Container>
+          )}
         </main>
       </PageWrapper>
     </div>
