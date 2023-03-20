f320da17a97d0f20dbbf6d03f2b1f7783f7d9021
Elton Gonçalves Gomes <eltongonc@gmail.com>
2023-03-20 13:05:46 +0100

    added text to the chat example

--- components/content-types/ChatExampleItem/ChatExampleItem.tsx
+++ components/content-types/ChatExampleItem/ChatExampleItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import styled from "styled-components";
 import hexToRgba from "hex-to-rgba";
 
@@ -51,6 +51,63 @@ const StyledForumPost = styled.article`
   }
 `;
 
+type ChatItem = {
+  dir: "to" | "from";
+  person: string;
+  message: string;
+};
+
+const convo: ChatItem[] = [
+  {
+    dir: "from",
+    person: "Fien",
+    message:
+      "Hoiii!! Ik ben nu bij mama en als ik ga slapen dan mis ik papa weer heel erg...",
+  },
+  {
+    dir: "to",
+    person: "Robin",
+    message:
+      "Ohh ja dat heb ik ook echt vaak gehad! Dat is echt geen fijn gevoel he :confused: Dan wil je eigenlijk van papa even een dikke knuffel maar dan kan het niet. Weet je wat mij dan hielp? Papa's shirt meenemen als ik naar mama ging en dan had hij zijn luchtje erop gespoten en ging ik daar lekker mee slapen :heart: Misschien kan je dat ook eens proberen?",
+  },
+  {
+    dir: "from",
+    person: "Fien",
+    message: "Dat ga ik doen!! Tnx :)",
+  },
+];
+
+function ChatBubble({
+  from,
+  children,
+  isLeft,
+}: {
+  from: string;
+  children: ReactNode;
+  isLeft: boolean;
+}) {
+  return (
+    <div
+      className="flex flex-col max-w-[75%]"
+      style={{
+        alignSelf: isLeft ? "flex-start" : "flex-end",
+      }}
+    >
+      <P
+        color="success"
+        style={{
+          textAlign: isLeft ? "left" : "right",
+          margin: 0,
+          fontWeight: 500,
+        }}
+      >
+        {from}
+      </P>
+      <div className="bg-white p-[12px] w-fit rounded-[8px]">{children}</div>
+    </div>
+  );
+}
+
 export default function ChatExampleItem() {
   return (
     <StyledForumPost>
@@ -65,10 +122,20 @@ export default function ChatExampleItem() {
             <P color="success" style={{ margin: 0, fontWeight: 500 }}>
               Buddy
             </P>
-            <P style={{ margin: 0, fontWeight: 300 }}>Mathijs, 18 jaar</P>
+            <P style={{ margin: 0, fontWeight: 300 }}>Robin, 18 jaar</P>
           </div>
         </header>
-        <div className="content"></div>
+        <div className="flex flex-col content max-h-[390px] overflow-y-auto">
+          {convo.map((item, index) => (
+            <ChatBubble
+              key={index}
+              from={item.person}
+              isLeft={item.dir === "from"}
+            >
+              {item.message}
+            </ChatBubble>
+          ))}
+        </div>
       </div>
       <footer>
         <Button variant="success" href="/vrijwilligerswerk/buddy-programma">
