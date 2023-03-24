fb3360bd0867fbebb83f2ce5bd48e60f4ed0724e
Elton Gonçalves Gomes <eltongonc@gmail.com>
2023-03-24 15:05:32 +0100

    moved the temporary quotes to the mockData file

--- pages/kinderen/ik-wil-een-buddy/index.tsx
+++ pages/kinderen/ik-wil-een-buddy/index.tsx
@@ -1,5 +1,5 @@
 import { FaAppStoreIos, FaGooglePlay } from "react-icons/fa";
-import { H1, H3, P, TitleWithHighlights } from "../../../components/typography";
+import { H3, P, TitleWithHighlights } from "../../../components/typography";
 
 import Button from "../../../components/buttons/Button";
 import CTAItem from "../../../components/content-types/CTAItem/CTAItem";
@@ -9,113 +9,15 @@ import FAQList from "../../../components/content-types/FAQList/FAQList";
 import { Hero } from "../../../components/layout";
 import PageWrapper from "../../../components/layout/PageWrapper/PageWrapper";
 import TextItem from "../../../components/content-types/TextItem/TextItem";
-import VideoItem from "../../../components/content-types/VideoItem/VideoItem";
 import parseImageURL from "../../../utils/parseImageURL";
 import { useTheme } from "styled-components";
 import ContentCarousel from "../../../components/carousels/ContentCarousel";
-import { Testimonial } from "../../../types/content-types/Testimonial.type";
-import { ContentStatus } from "../../../types/content-types/Status.type";
+import { TEMP_QUOTES } from "../../../constants/mockData";
 
 type BuddyPageProps = {
   pageData: any;
   error?: boolean;
 };
-
-const TEMP_QUOTES: Testimonial[] = [
-  {
-    id: "test",
-    title: "Jongen (14) over zijn Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Ze zorgt ervoor dat ik rustiger ben en niet met stomme dingen in mijn hoofd zit. Dat ik het meteen kan zeggen en alles kwijt kan wat mij dwars zit over de scheiding, en ook over andere dingen. Ik vind het fijn dat ik mijn Buddy daarvoor heb.",
-  },
-  {
-    id: "test",
-    title: "Meisje (19) over haar Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Het is fijn weer even m'n hart te kunnen luchten. De meeste mensen die ik tot nu toe heb gesproken, begrepen het namelijk niet écht. Dan kunnen ze het nog zo lief bedoelen, maar het geeft niet het gevoel dat je begrepen wordt, en dat voelt zo rot. Dankjewel voor al je lieve berichtjes steeds.",
-  },
-  {
-    id: "test",
-    title: "Meisje (12) tegen haar Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Ik heb eigenlijk niemand om mee te praten nu over hoe ik me echt voel behalve jij.",
-  },
-
-  {
-    id: "test",
-    title: "Meisje (21) over haar Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Ik ben zo blij dat ik dingen met mijn Buddy kan delen. Ik weet wel dat ze het niet kan oplossen, maar doordat ik het inspreek is het wel weer uit mijn hoofd en ik weet dat zij er niet over zal oordelen, bedankt daarvoor!",
-  },
-
-  {
-    id: "test",
-    title: "Jongen (12) tegen zijn Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Ik heb super goed nieuws, want toen ik net jou als Buddy kreeg voelde ik me echt niet goed en gewoon super slecht maar nu sinds ik al m'n gevoel heb verteld aan m'n vader voel ik me beter!",
-  },
-
-  {
-    id: "test",
-    title: "Jongen (12) over zijn Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Ze zorgt ervoor dat ik rustiger ben en niet met stomme dingen in mijn hoofd zit. Dat ik het meteen kan zeggen en alles kwijt kan wat mij dwars zit over de scheiding, en ook over andere dingen. Ik vind het fijn dat ik mijn Buddy daarvoor heb. Iemand waar ik contact mee heb, maar ook gewoon lekker mee kan kletsen.",
-  },
-
-  {
-    id: "test",
-    title: "Meisje (21) tegen haar Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Lieve Buddy, ondanks dat we elkaar nog helemaal niet zo lang kennen wil ik je toch laten weten dat je in korte tijd al veel voor me hebt betekend. Het voelt echt alsof ik een zus heb die precies weet wat ik voel. Je geeft me zelfvertrouwen. Dankjewel!",
-  },
-
-  {
-    id: "test",
-    title: "Meisje (10) tegen haar Buddy:",
-    author: "",
-    date: "",
-    date_updated: "",
-    user_updated: "villa pinedo",
-    status: "published" as ContentStatus,
-    description:
-      "Maar nu ben jij een soort van mijn dagboek en het coole van dit dagboek is dat ze terug praat!",
-  },
-];
 
 export const getServerSideProps = async () => {
   // fetch page data from API
